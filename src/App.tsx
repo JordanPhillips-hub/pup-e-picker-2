6b87cb2efba4431526738907c71b33ffb402db14
Jordan Phillips <jordan.phillips.988@gmail.com>
2024-05-27 15:27:13 -0600

    Refactor app
    Remove dog provider
    
    import CreateDogForm and useNavigationContext, set conditional rendering for dogs and create dog form

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import { DogProvider } from "./providers/DogProvider";
 import { Section } from "./Components/Section";
 import { Dogs } from "./Components/Dogs";
+import { CreateDogForm } from "./Components/CreateDogForm";
+import { useNavigationContext } from "./providers/NavigationProvider";
 
 export function App() {
+  const { currentView } = useNavigationContext();
   return (
-    <DogProvider>
-      <div className="App" style={{ backgroundColor: "skyblue" }}>
-        <header>
-          <h1>pup-e-picker (Functional)</h1>
-        </header>
-        <Section label={"Dogs: "}>
-          <Dogs />
-        </Section>
-      </div>
-    </DogProvider>
+    <div className="App" style={{ backgroundColor: "skyblue" }}>
+      <header>
+        <h1>pup-e-picker (Functional)</h1>
+      </header>
+      <Section label={"Dogs: "}>
+        {currentView === "create dog" ? <CreateDogForm /> : <Dogs />}
+      </Section>
+    </div>
   );
 }
