2ec1c53b9cfa93ad48eb6010775c1243aeb873a1
Jordan Phillips <jordan.phillips.988@gmail.com>
2024-05-27 15:28:24 -0600

    Refactor main
    Import DogProvider and NavigationProvider
    
    Wrap App inside providers

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Toaster } from "react-hot-toast";
+import { App } from "./App";
+import { NavigationProvider } from "./providers/NavigationProvider";
+import { DogProvider } from "./providers/DogProvider";
 import "./index.css";
 import "./App.css";
 import "./index.css";
-import { Toaster } from "react-hot-toast";
-import { App } from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Toaster />
-    <App />
+    <DogProvider>
+      <NavigationProvider>
+        <App />
+      </NavigationProvider>
+    </DogProvider>
   </React.StrictMode>
 );
