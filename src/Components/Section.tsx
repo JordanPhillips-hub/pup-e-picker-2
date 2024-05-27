62a6ab77cbfc4d9b75248b1ce4d5af39c8140b1b
Jordan Phillips <jordan.phillips.988@gmail.com>
2024-05-27 15:37:29 -0600

    Refactor Section
    Refactor to map and handle navigation tabs

--- src/Components/Section.tsx
+++ src/Components/Section.tsx
@@ -1,45 +1,50 @@
 import { ReactNode } from "react";
+import { useDogContext } from "../providers/DogProvider";
+import { useNavigationContext } from "../providers/NavigationProvider";
+import { TActiveTab } from "../types";
 
 export const Section = ({
   label,
   children,
 }: {
-  // No more props than these two allowed
   label: string;
   children: ReactNode;
 }) => {
+  const { dogs } = useDogContext();
+  const { currentView, setCurrentView } = useNavigationContext();
+
+  const favoritedDogs = dogs.filter((dog) => dog.isFavorite);
+  const unfavoritedDogs = dogs.filter((dog) => !dog.isFavorite);
+
+  const navigationTabs = [
+    { name: "favorited", counter: `(${favoritedDogs.length})` },
+    { name: "unfavorited", counter: `(${unfavoritedDogs.length})` },
+    { name: "create dog" },
+  ];
+
+  const handleCurrentView = (tab: string) => {
+    setCurrentView(tab as TActiveTab);
+    if (currentView === tab) setCurrentView("allDogs");
+  };
+
   return (
     <section id="main-section">
       <div className="container-header">
         <div className="container-label">{label}</div>
         <div className="selectors">
-          {/* This should display the favorited count */}
-          <div
-            className={`selector ${"active"}`}
-            onClick={() => {
-              alert("click favorited");
-            }}
-          >
-            favorited ( {0} )
-          </div>
-
-          {/* This should display the unfavorited count */}
-          <div
-            className={`selector ${""}`}
-            onClick={() => {
-              alert("click unfavorited");
-            }}
-          >
-            unfavorited ( {10} )
-          </div>
-          <div
-            className={`selector ${""}`}
-            onClick={() => {
-              alert("clicked create dog");
-            }}
-          >
-            create dog
-          </div>
+          {navigationTabs.map((tab) => (
+            <div
+              key={tab.name}
+              // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+              className={`selector ${currentView === tab.name && "active"}`}
+              onClick={() => handleCurrentView(tab.name)}
+            >
+              {tab.name === "favorited" || tab.name === "unfavorited"
+                ? // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+                  `${tab.name} ${tab.counter}`
+                : tab.name}
+            </div>
+          ))}
         </div>
       </div>
       <div className="content-container">{children}</div>
