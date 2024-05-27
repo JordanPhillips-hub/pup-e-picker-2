83adce4da270e99dd5995c5d1f092a36fc395f54
Jordan Phillips <jordan.phillips.988@gmail.com>
2024-05-27 13:59:06 -0600

    Import and use DogProvider / Dogs component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
+import { DogProvider } from "./providers/DogProvider";
 import { Section } from "./Components/Section";
+import { Dogs } from "./Components/Dogs";
 
 export function App() {
   return (
-    <div className="App" style={{ backgroundColor: "skyblue" }}>
-      <header>
-        <h1>pup-e-picker (Functional)</h1>
-      </header>
-      <Section label={"Dogs: "}>
-
-      </Section>
-    </div>
+    <DogProvider>
+      <div className="App" style={{ backgroundColor: "skyblue" }}>
+        <header>
+          <h1>pup-e-picker (Functional)</h1>
+        </header>
+        <Section label={"Dogs: "}>
+          <Dogs />
+        </Section>
+      </div>
+    </DogProvider>
   );
 }
