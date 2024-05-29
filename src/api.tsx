ff92ec3a7c85673a3299908a9a5d55151ca6ef62
Jordan Phillips <jordan.phillips.988@gmail.com>
2024-05-29 10:06:19 -0600

    Update promise void types

--- src/api.tsx
+++ src/api.tsx
@@ -13,20 +13,18 @@ const postDog = (dog: Omit<Dog, "id">): Promise<Dog> =>
     },
   }).then((res): Promise<Dog> => res.json());
 
-const deleteDogRequest = (id: number): Promise<void> =>
+const deleteDogRequest = (id: number): Promise<Dog> =>
   fetch(`${baseUrl}/dogs/${id}`, {
     body: "",
     method: "Delete",
-  }).then((res): Promise<void> => res.json());
+  }).then((res): Promise<Dog> => res.json());
 
-const patchFavoriteForDog = (id: number, isFavorite: boolean): Promise<void> =>
+const patchFavoriteForDog = (id: number, isFavorite: boolean): Promise<Dog> =>
   fetch(`${baseUrl}/dogs/${id}`, {
     body: JSON.stringify({ isFavorite: isFavorite }),
     method: "PATCH",
     headers: { "Content-Type": "application/json" },
-  })
-    .then((res): Promise<void> => res.json())
-    .catch((err) => console.log(err));
+  }).then((res): Promise<Dog> => res.json());
 
 export const Requests = {
   postDog,
