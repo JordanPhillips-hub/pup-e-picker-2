fae1881c3741414bb304f568d8020cf60598e8aa
Jordan Phillips <jordan.phillips.988@gmail.com>
2024-05-29 08:38:58 -0600

    Create deleteDogRequest and patchFavoriteForDog

--- src/api.tsx
+++ src/api.tsx
@@ -11,15 +11,22 @@ const postDog = (dog: Omit<Dog, "id">): Promise<Dog> =>
     headers: {
       "Content-Type": "application/json",
     },
-  }).then((response): Promise<Dog> => response.json());
+  }).then((res): Promise<Dog> => res.json());
 
-const deleteDogRequest = () => {
-  // fill out method
-};
+const deleteDogRequest = (id: number): Promise<void> =>
+  fetch(`${baseUrl}/dogs/${id}`, {
+    body: "",
+    method: "Delete",
+  }).then((res): Promise<void> => res.json());
 
-const patchFavoriteForDog = () => {
-  // fill out method
-};
+const patchFavoriteForDog = (id: number, isFavorite: boolean): Promise<void> =>
+  fetch(`${baseUrl}/dogs/${id}`, {
+    body: JSON.stringify({ isFavorite: isFavorite }),
+    method: "PATCH",
+    headers: { "Content-Type": "application/json" },
+  })
+    .then((res): Promise<void> => res.json())
+    .catch((err) => console.log(err));
 
 export const Requests = {
   postDog,
