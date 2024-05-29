4f1ef4ba1e6bd785d784baa7b1551e9286ec8d6a
Jordan Phillips <jordan.phillips.988@gmail.com>
2024-05-29 11:42:35 -0600

    Create handleRequest function update functions to use handleRequest

--- src/providers/DogProvider.tsx
+++ src/providers/DogProvider.tsx
@@ -10,32 +10,27 @@ type TDogContext = {
 };
 
 export const DogContext = createContext<TDogContext>({} as TDogContext);
-
 export const DogProvider = ({ children }: { children: ReactNode }) => {
   const [dogs, setDogs] = useState<Dog[]>([]);
 
   const fetchData = () => Requests.getAllDogs().then(setDogs);
-
-  const createDog = async (dog: Omit<Dog, "id">): Promise<Dog> => {
-    const newDog = await Requests.postDog(dog);
+  const handleRequest = async (promise: Promise<Dog>) => {
+    const request = await promise;
     await fetchData();
-    return newDog;
+    return request;
   };
 
-  const deleteDog = async (id: number): Promise<Dog> => {
-    const updatedDogs = await Requests.deleteDogRequest(id);
-    await fetchData();
-    return updatedDogs;
-  };
+  const createDog = (dog: Omit<Dog, "id">): Promise<Dog> =>
+    handleRequest(Requests.postDog(dog));
 
-  const updateDog = async (id: number, isFavorite: boolean): Promise<Dog> => {
-    const updatedDog = await Requests.patchFavoriteForDog(id, isFavorite);
-    await fetchData();
-    return updatedDog;
-  };
+  const deleteDog = (id: number): Promise<Dog> =>
+    handleRequest(Requests.deleteDogRequest(id));
+
+  const updateDog = (id: number, isFavorite: boolean): Promise<Dog> =>
+    handleRequest(Requests.patchFavoriteForDog(id, isFavorite));
 
   useEffect(() => {
-    fetchData().catch((err) => console.log(err));
+    fetchData().catch((err) => console.error("Error fetching dogs", err));
   }, []);
 
   return (
