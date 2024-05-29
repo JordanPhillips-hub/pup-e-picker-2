7f095c32d1432f653171fd5ce2dc80362ecfbe50
Jordan Phillips <jordan.phillips.988@gmail.com>
2024-05-29 10:19:30 -0600

    Move useNavigationContext to hooks folder

--- src/providers/NavigationProvider.tsx
+++ src/providers/NavigationProvider.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, createContext, useContext, useState } from "react";
+import { ReactNode, createContext, useState } from "react";
 import { TActiveTab } from "../types";
 
 type TNavigationContext = {
@@ -6,11 +6,10 @@ type TNavigationContext = {
   setCurrentView: React.Dispatch<React.SetStateAction<TActiveTab>>;
 };
 
-const NavigationContext = createContext<TNavigationContext>(
+export const NavigationContext = createContext<TNavigationContext>(
   {} as TNavigationContext
 );
 
-export const useNavigationContext = () => useContext(NavigationContext);
 export const NavigationProvider = ({ children }: { children: ReactNode }) => {
   const [currentView, setCurrentView] = useState<TActiveTab>("allDogs");
   return (
