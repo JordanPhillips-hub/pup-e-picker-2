2451652f2b9ab5100917e350daf086b5d9db4c47
Jordan Phillips <jordan.phillips.988@gmail.com>
2024-06-01 10:15:21 -0600

    Update deleteDog, createDog, updateDog functions

--- src/providers/DogProvider.tsx
+++ src/providers/DogProvider.tsx
@@ -13,24 +13,32 @@ export const DogContext = createContext<TDogContext>({} as TDogContext);
 export const DogProvider = ({ children }: { children: ReactNode }) => {
   const [dogs, setDogs] = useState<Dog[]>([]);
 
-  const fetchData = () => Requests.getAllDogs().then(setDogs);
-  const handleRequest = async (promise: Promise<Dog>) => {
-    const request = await promise;
-    await fetchData();
-    return request;
+  // Set Loading State
+  const createDog = (dog: Omit<Dog, "id">): Promise<Dog> =>
+    Requests.postDog(dog);
+
+  const deleteDog = (id: number): Promise<Dog> => {
+    setDogs(dogs.filter((dog) => dog.id !== id));
+    return Requests.deleteDogRequest(id).catch((error: Error) => {
+      setDogs(dogs);
+      throw error;
+    });
   };
 
-  const createDog = (dog: Omit<Dog, "id">): Promise<Dog> =>
-    handleRequest(Requests.postDog(dog));
-
-  const deleteDog = (id: number): Promise<Dog> =>
-    handleRequest(Requests.deleteDogRequest(id));
-
-  const updateDog = (id: number, isFavorite: boolean): Promise<Dog> =>
-    handleRequest(Requests.patchFavoriteForDog(id, isFavorite));
+  const updateDog = (id: number, isFavorite: boolean): Promise<Dog> => {
+    setDogs(dogs.map((dog) => (dog.id === id ? { ...dog, isFavorite } : dog)));
+    return Requests.patchFavoriteForDog(id, isFavorite).catch(
+      (error: Error) => {
+        setDogs(dogs);
+        throw error;
+      }
+    );
+  };
 
   useEffect(() => {
-    fetchData().catch((err) => console.error("Error fetching dogs", err));
+    Requests.getAllDogs()
+      .then(setDogs)
+      .catch((err) => console.error("Error fetching dogs", err));
   }, []);
 
   return (
