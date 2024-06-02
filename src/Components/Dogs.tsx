ca7da34f3aae9e2e3146deb821d9abe207ff7ad0
Jordan Phillips <jordan.phillips.988@gmail.com>
2024-06-02 13:20:56 -0600

    Refactor Dogs

--- src/Components/Dogs.tsx
+++ src/Components/Dogs.tsx
@@ -1,21 +1,14 @@
 import { DogCard } from "./DogCard";
 import { useDogContext, useNavigationContext } from "../hooks/api";
-import toast from "react-hot-toast";
 
 export const Dogs = () => {
-  const { dogs, deleteDog, updateDog } = useDogContext();
+  const { isLoading, dogs, deleteDog, updateDog } = useDogContext();
   const { currentView } = useNavigationContext();
 
   const filteredDogs = dogs.filter((dog) => {
     if (currentView === "favorited") return dog.isFavorite;
     if (currentView === "unfavorited") return !dog.isFavorite;
     return true;
-  });
-
-  const messages = (name: string) => ({
-    deleted: `${name} has been deleted`,
-    favorite: `${name} added to favorites`,
-    unFavorite: `${name} removed from favorites`,
   });
 
   return (
@@ -30,19 +23,10 @@ export const Dogs = () => {
             isFavorite: isFavorite,
             name: name,
           }}
-          onTrashIconClick={() => {
-            toast.success(messages(name).deleted);
-            return deleteDog(id);
-          }}
-          onHeartClick={() => {
-            toast.success(messages(name).unFavorite);
-            return updateDog(id, !isFavorite);
-          }}
-          onEmptyHeartClick={() => {
-            toast.success(messages(name).favorite);
-            return updateDog(id, !isFavorite);
-          }}
-          isLoading={false}
+          onTrashIconClick={() => deleteDog(id, name)}
+          onHeartClick={() => updateDog(id, !isFavorite, name)}
+          onEmptyHeartClick={() => updateDog(id, !isFavorite, name)}
+          isLoading={isLoading}
         />
       ))}
     </>
