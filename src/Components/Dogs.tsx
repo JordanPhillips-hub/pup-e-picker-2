4df44092cb6185827db594cbaf900e6627e6974f
Jordan Phillips <jordan.phillips.988@gmail.com>
2024-06-04 14:22:38 -0600

    Refactor dogs.tsx

--- src/Components/Dogs.tsx
+++ src/Components/Dogs.tsx
@@ -1,34 +1,45 @@
 import { DogCard } from "./DogCard";
-import { useDogContext, useNavigationContext } from "../hooks/api";
+import { useDogContext, useNavigationContext } from "../hooks/api.hooks";
+import { TActiveTab, Dog } from "../types";
 
 export const Dogs = () => {
-  const { isLoading, dogs, deleteDog, updateDog } = useDogContext();
   const { currentView } = useNavigationContext();
+  const {
+    isLoading,
+    dogs,
+    favoritedDogs,
+    unfavoritedDogs,
+    deleteDog,
+    updateDog,
+  } = useDogContext();
 
-  const filteredDogs = dogs.filter((dog) => {
-    if (currentView === "favorited") return dog.isFavorite;
-    if (currentView === "unfavorited") return !dog.isFavorite;
-    return true;
-  });
+  const dogsList: Record<TActiveTab, Dog[]> = {
+    favorited: favoritedDogs,
+    unfavorited: unfavoritedDogs,
+    allDogs: dogs,
+    "create dog": [],
+  };
 
   return (
     <>
-      {filteredDogs.map(({ id, image, description, isFavorite, name }) => (
-        <DogCard
-          key={id}
-          dog={{
-            id: id,
-            image: image,
-            description: description,
-            isFavorite: isFavorite,
-            name: name,
-          }}
-          onTrashIconClick={() => deleteDog(id, name)}
-          onHeartClick={() => updateDog(id, !isFavorite, name)}
-          onEmptyHeartClick={() => updateDog(id, !isFavorite, name)}
-          isLoading={isLoading}
-        />
-      ))}
+      {dogsList[currentView].map(
+        ({ id, image, description, isFavorite, name }) => (
+          <DogCard
+            key={id}
+            dog={{
+              id: id,
+              image: image,
+              description: description,
+              isFavorite: isFavorite,
+              name: name,
+            }}
+            onTrashIconClick={() => deleteDog(id, name)}
+            onHeartClick={() => updateDog(id, !isFavorite, name)}
+            onEmptyHeartClick={() => updateDog(id, !isFavorite, name)}
+            isLoading={isLoading}
+          />
+        )
+      )}
     </>
   );
 };
