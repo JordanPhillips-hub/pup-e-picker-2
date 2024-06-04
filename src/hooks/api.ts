d32f8826ab459e76e608afda643da3bbf3c49764
Jordan Phillips <jordan.phillips.988@gmail.com>
2024-06-04 14:23:30 -0600

    Rename hooks api file

--- src/hooks/api.ts
+++
@@ -1,6 +0,0 @@
-import { useContext } from "react";
-import { NavigationContext } from "../providers/NavigationProvider";
-import { DogContext } from "../providers/DogProvider";
-
-export const useNavigationContext = () => useContext(NavigationContext);
-export const useDogContext = () => useContext(DogContext);
